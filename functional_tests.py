f0cc0eb79cd17f139b8f4cd3ca25a08a555954d7
Maxim <maxshaptala@gmail.com>
2017-01-30 00:14:08 +0200

    Начало списка задач при разработке

--- functional_tests.py
+++ functional_tests.py
@@ -1,10 +1,17 @@
 from selenium import webdriver
 
 browser = webdriver.Firefox(executable_path='e:\geckodriver.exe')
-try:
-    browser.get('http://localhost:8000')
-    assert 'Django' in browser.title
-except Exception as ex:
-    print(ex)
-finally:
-    browser.quit()
+
+# Edith has heard about a copy new online to-do app.
+# She goes to check out its homepage
+browser.get('http://localhost:8000')
+
+# She notice the page title a heard mention to-do list
+assert 'Django' in browser.title
+
+# Sh is invited to enter a to-do item straight away
+
+# She types 'Buy peacock feathers" into a text box
+# (Edith's hobby is tying fly-finishing lures)
+
+# TODO: complete tasks
