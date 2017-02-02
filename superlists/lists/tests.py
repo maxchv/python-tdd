560268d3b7b375c9cb87af77c702026870286e71
Maxim <maxshaptala@gmail.com>
2017-02-02 15:04:51 +0200

    Убраны лишние import

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -1,6 +1,6 @@
-from django.http import HttpRequest
-from django.test import TestCase
 from django.core.urlresolvers import resolve
+from django.test import TestCase
+
 from lists.views import home_page
 
 
