0450815b86a8315b7b1f700517d1fd1c0a7bb5cf
Maxim <maxshaptala@gmail.com>
2017-02-02 14:57:53 +0200

    рефакторинг метода проверки домашней страницы

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -10,9 +10,11 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
+        response = self.client.get('/')
+
         html = response.content.decode('utf-8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.endswith("</html>"))
+
+        self.assertTemplateUsed(response, 'home.html')
