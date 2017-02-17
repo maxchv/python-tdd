5a72ccd95db1606d30fe1be38b53db635ead0d12
Maxim <maxshaptala@gmail.com>
2017-02-18 01:23:32 +0200

    Добавлена проверка шаблона представления

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -24,3 +24,4 @@ class HomePageTest(TestCase):
     def test_can_save_a_post_request(self):
         response = self.client.post('/', data={"item_text": 'A new list item'})
         self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
