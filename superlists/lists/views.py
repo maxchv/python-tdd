45f0359003f2cc2898c2008d1aa579cdef8887e2
Maxim <maxshaptala@gmail.com>
2017-02-18 02:13:58 +0200

    модульый тест проходит

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,8 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Item
 
 
 def home_page(request):
+    item = Item()
+    item.text = request.POST.get('item_text', '')
+    item.save()
+
     return render(request, 'home.html', {
         'new_item_text': request.POST.get('item_text')
     })
