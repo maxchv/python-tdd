9a8444ccbe15e54d65a150ce8f0a0b7c34c20c29
Maxim <maxshaptala@gmail.com>
2017-02-27 00:47:03 +0200

    добавлены Unit тесты для проверки сохранения сообщения в базу данных

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -34,9 +34,19 @@ class HomePageTest(TestCase):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
 
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
 
 class ItemTest(TestCase):
-    def test_saving_and_retieving_items(self):
+    def test_saving_and_retrieving_items(self):
         first_item = Item()
         first_item.text = 'The first (ever) list item'
         first_item.save()
